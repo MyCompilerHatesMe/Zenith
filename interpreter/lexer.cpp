ba6de5e0379408907dd088a3e6c39ced431e87ca
My Compiler Hates Me <acjha990@gmail.com>
2025-10-12 20:51:38 +0530

    Added remaining helper methods
    Finished number and identifier.

--- interpreter/lexer.cpp
+++ interpreter/lexer.cpp
@@ -74,11 +74,11 @@ Token Lexer::string(){
     start = current; // store where the string starts. you called the method so it must be here
     while(peek() != '"' && !isAtEnd()){
         if(peek() == '\n') line++;
-        advance();
         if(peek() == '"'){
-            advance();
+            advance(); // remove the " from the stream
             return makeToken(STRING); // make token will figure out where we are right now and return what we need
         }
+        advance();
     }
 
     if(isAtEnd()){
@@ -86,3 +86,38 @@ Token Lexer::string(){
         std::exit(1);  // error means fuck you get out
     }
 }
+
+Token Lexer::number(){
+    start = current;
+    while(isDigit(peek())){
+        if(peek() == '\n'){
+            cerr << "Missing ';' at line" << line << "\n";
+            std::exit(1);
+        }
+        advance();
+    }
+    return makeToken(NUMBER);
+}
+
+Token Lexer::identifier(){
+    start = current;
+    while(isAlphaNumeric(peek())){
+        if(peek() == '\n'){
+            cerr << "Missing ';' at line " << line << "\n";
+            std::exit(1);
+        }
+        advance();
+    }
+
+    // find the identifiers contents.
+    std::string text = source.substr(start, current - start);
+
+    auto it = keywords.find(text);
+    
+    //if its in the map return a keyword type
+    if(it != keywords.end()) return makeToken(it->second);
+    //if its not in the map, return an identifier
+    return makeToken(IDENTIFIER);
+}
+
+//TODO: makeToken.
