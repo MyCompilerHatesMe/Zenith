e9a852694a939d1f0c2a3e2d01ae22ffc7f8147e
My Compiler Hates Me <acjha990@gmail.com>
2025-10-12 23:41:33 +0530

    Had to redo some of them
    Accidentally created an infinite loop of helpers calling each other lol. peek and previous access the token stream directly now

--- interpreter/parser/evaluator.cpp
+++ interpreter/parser/evaluator.cpp
@@ -9,28 +9,27 @@
 #include "evaluator.h"
 
 
-
-// navigation
+// only use this to access token stream
 Token Parser::peek() const {
-    return tokens[current];
+    return tokens.at(current);
 }
 
-Token Parser::advance() {
-    if(!isAtEnd()) return tokens[current++];
-}
-
+// this works
 bool Parser::isAtEnd() const {
     return peek().type == END_OF_FILE;
 }
 
 Token Parser::previous() const {
-    //make sure current >= 1 still using at
     return tokens.at(current - 1);
 }
 
-// check and consume
+Token Parser::advance() {
+    if (!isAtEnd()) current++;
+    return previous();
+}
+
 bool Parser::match(TokenType type) {
-    if(check(type)) {
+    if (check(type)) {
         advance();
         return true;
     }
@@ -38,12 +37,13 @@ bool Parser::match(TokenType type) {
 }
 
 bool Parser::check(TokenType type) const {
-    if(!isAtEnd()) return false;
+    if (isAtEnd()) return false;
     return peek().type == type;
 }
 
 Token Parser::consume(TokenType type, const std::string& message) {
-    if(check(type)) return advance();
+    if (check(type)) return advance();
+
     std::cerr << "[line " << peek().line << "] Error: " << message << std::endl;
     std::exit(1);
 }
