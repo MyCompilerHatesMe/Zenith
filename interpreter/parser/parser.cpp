1ea971ef52e0389c3e22f7dc3f3a53e20f55f50c
PranavNihal <pranavnihal001@gmail.com>
2025-10-13 11:06:02 +0530

    Renaming to Parser

---
+++ interpreter/parser/parser.cpp
@@ -0,0 +1,6 @@
+#define PARSER_h
+#include <string>
+#include <vector>
+#include <memory>
+#include <stdexcept>
+#include "interpreter/token.h"
---
+++ interpreter/parser/parser.h
@@ -0,0 +1,47 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "interpreter/token.h"
+#include <memory>
+#include <string>
+#include <vector>
+struct Expression {
+    virtual ~Expression() = default;
+};
+struct BinaryExpression : Expression {
+    std::unique_ptr<Expression> left;
+    Token op;
+    std::unique_ptr<Expression> right;
+};
+struct UnaryExpression : Expression {
+    Token op;
+    std::unique_ptr<Expression> expr;
+};
+struct LiteralExpression : Expression {
+    Token op;
+};
+
+class Parser {
+    private:
+    std::vector<Token> tokens;
+    int current;
+    public:
+    Parser(const std::vector<Token>& tokens) : tokens(tokens), current(0) {}
+    // Parses the tokens and returns the root of the AST
+    std::unique_ptr<Expression> parseExpression();
+    private:
+        Token peek() const;
+        Token advance();
+        bool match(TokenType type);
+        Token consume(TokenType type, const std::string& message);
+        bool check(TokenType type) const;
+        bool isAtEnd();
+
+        //Levels of Precedence
+        std::unique_ptr<Expression> parseAddition();
+        std::unique_ptr<Expression> parseMultiplication();
+        std::unique_ptr<Expression> parseUnary();
+        std::unique_ptr<Expression> parsePrimary();
+};
+
+#endif 
